9bf4c9910d90ad432cea0c969fcc5d795cc9932e
Gaurav Dubey <gaurav.dubey@thewitslab.com>
2023-06-13 07:40:34 +0000

    Delete Constants.java

--- test-automation-framework/src/main/java/com/automation/ui/selenium/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.automation.ui.selenium;
-
-public class Constants {
-    
-    public static final String GMAIL_EMAIL_USER_NAME = "testuser@gmail.com";
-    public static final String GMAIL_EMAIL_USER_PASS = "test@123";
-    
-}
